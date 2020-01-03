fde00838f8a3bb04b7124c2a4c82ac3a45988a29
Carlos Castro Brenes <ccasbre27@hotmail.com>
2020-01-03 14:22:15 -0600

    added conditional message in list appointment section

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -3,10 +3,14 @@ import Appointment from './Appointment';
 import PropTypes from 'prop-types';
 
 const ListAppointments = ({ appointments, deleteAppointment }) => {
+
+    // imprimir un mensaje si hay citas o no
+    const message = Object.keys(appointments).length === 0 ? 'There are not appointments yet :(' : 'Manage your appointments';
+
     return (
         <div className="card mt-2 py-5">
             <div className="card-body">
-                <h2 className="card-title text-center">Manage your appointments</h2>
+                <h2 className="card-title text-center">{message}</h2>
                 <div className="lista-citas">
                     {appointments.map(app => (
                         <Appointment
